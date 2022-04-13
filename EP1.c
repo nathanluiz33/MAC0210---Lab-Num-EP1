9b806a8baa5ea33621537e21923823946e2e1cfe
Nathan Luiz <nathanluiz33@gmail.com>
2022-04-13 19:18:43 -0300

    Atualizando parte 1

--- EP1.c
+++ EP1.c
@@ -4,6 +4,24 @@
 
 const double EPS = 1e-8;
 
+// g(x) = x <-> f(x) = 0
+double ABS(double x){
+    if(x > -EPS) return x;
+    return -x;
+}
+
+double metodo_ponto_fixo(double x0, double (*f)(double), double (*G)(double)){
+    double raiz = x0;
+    int limite_iteracoes = 100;
+    while((ABS(f(raiz)) > EPS || ABS(raiz - G(raiz)) > EPS) && limite_iteracoes > 0){
+        // printf("%f\n", raiz);
+        limite_iteracoes--;
+        raiz = G(raiz);
+    }
+    if(limite_iteracoes == 0) return NAN;
+    return raiz;
+}
+
 double f(double x){
     return exp(x) - 2 * x * x;
 }
@@ -18,16 +36,10 @@ double g1(double x){
     return -f(x) / 2 + x;
 }
 
-double ABS(double x){
-    if(x > -EPS) return x;
-    return -x;
-}
-
 int main(){
     double raiz = 1;
-    while(ABS(f(raiz)) > EPS || ABS(raiz - g(raiz)) > EPS){
-        // printf("%f\n", raiz);
-        raiz = g(raiz);
-    }
-    printf("%.8f   %.8f", raiz, f(raiz));
+    raiz = metodo_ponto_fixo(raiz, f, g);
+    printf("A primeira raiz é: %.8f\n", metodo_ponto_fixo(1, f, g));
+    printf("A segunda raiz é: %.8f\n", metodo_ponto_fixo(2, f, g1));
+    printf("A terceira raiz é: %.8f\n", metodo_ponto_fixo(1, f, g1));
 }
